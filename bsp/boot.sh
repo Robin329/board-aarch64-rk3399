8aa5c3d18d816456c6a83a16cbcdc108ea1c3642
Robin <jiangrenbin329@gmail.com>
2020-02-07 03:16:47 -0500

    Add boot.sh
    Signed-off-by: Robin <jiangrenbin329@gmail.com>

---
+++ bsp/boot.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: boot.sh
+#
+#         USAGE: ./boot.sh
+#
+#   DESCRIPTION: copy Image & dtb to ~/tftpboot dir.
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Robin.J
+#  ORGANIZATION: 
+#       CREATED: 2020年02月07日 06时51分09秒
+#      REVISION:  V01
+#===============================================================================
+
+set -o nounset                                  # Treat unset variables as an error
+PWD=`pwd`
+
+# copy Image and dtb to ~/tftpboot dir.
+echo
+echo -e "\033[31m Start Copy Image & dtb to ~/tftpboot ...\033[0m"
+echo
+sudo cp ~/workspace/project/docker/cloud-lab/labs/linux-lab/output/aarch64/linux-v5.5-rk3399-roc-pc/arch/arm64/boot/Image ~/tftpboot
+sudo cp ~/workspace/project/docker/cloud-lab/labs/linux-lab/output/aarch64/linux-v5.5-rk3399-roc-pc/arch/arm64/boot/dts/rockchip/rk3399-roc-pc.dtb ~/tftpboot
+echo
+echo -e "\033[31m Copy finished !!!\033[0m"
+
